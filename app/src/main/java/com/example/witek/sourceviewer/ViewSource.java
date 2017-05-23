b17bfcf80b893f74b89b8c7eb2f358160ec7ed85
Witold Leicht <witold.leicht@gmail.com>
2017-05-23 20:54:54 +0200

    Added the progress bar

--- app/src/main/java/com/example/witek/sourceviewer/ViewSource.java
+++ app/src/main/java/com/example/witek/sourceviewer/ViewSource.java
@@ -3,6 +3,8 @@ package com.example.witek.sourceviewer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,15 +18,15 @@ import org.jsoup.nodes.Document;
 public class ViewSource extends AppCompatActivity {
 
     TextView dispUrl;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_source);
 
-        //ConnectionHelper.getInstance(this);
-
         dispUrl  = (TextView) findViewById(R.id.displayUrl);
-
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         Intent intent = getIntent();
         String url = intent.getExtras().getString("address");
@@ -32,6 +34,7 @@ public class ViewSource extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 Document doc = Jsoup.parse(response);
+                progressBar.setVisibility(View.GONE);
                 dispUrl.setText(doc.toString());
 
             }
@@ -53,5 +56,6 @@ public class ViewSource extends AppCompatActivity {
         //TODO create a table in the database
         //TODO save the raw string obtained from Volley to the database
         //TODO handle any errors
+        
     }
 }
